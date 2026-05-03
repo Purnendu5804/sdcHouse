4813e17d240183fcb2388291ca745fe3da4dd4de
Purnendu Tiwari <purnendutiwari2004@gmail.com>
2026-05-03 11:28:50 +0530

    update cors origin

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 const io = new Server(server , {
   cors : {
-    origin: "*",
+    origin: "https://sdc-house.vercel.app/",
     methods: ["GET" , "POST"]
   }
 });
